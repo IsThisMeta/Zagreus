664cd582def9605a14f5ee53e24a44adee0d002e
Cody <cjsclark@icloud.com>
2025-09-10 02:17:19 -0600

    Delete fix_device_tokens_constraint.sql

--- zagreus-notification-service/fix_device_tokens_constraint.sql
+++
@@ -1,9 +0,0 @@
--- Add the missing unique constraint on device_tokens table
-ALTER TABLE device_tokens 
-ADD CONSTRAINT device_tokens_user_id_token_unique 
-UNIQUE (user_id, token);
-
--- Verify the constraint was added
-SELECT conname, contype 
-FROM pg_constraint 
-WHERE conrelid = 'device_tokens'::regclass;
